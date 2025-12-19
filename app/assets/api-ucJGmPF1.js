faabc03eae1fe4a33c8dde0c6d3fc8c0b0d35038
Vardan2009 <Vardan2009@users.noreply.github.com>
2025-12-19 13:10:13 +0000

    deploy: b9e8c924ea48c0e2c499de64af9ce83cdf4927aa

--- app/assets/api-ucJGmPF1.js
+++ app/assets/api-ucJGmPF1.js
@@ -1,3 +1,3 @@
-import{s as e}from"./index-BT_MWNEU.js";const s=`https://time-manager-app-backend.onrender.com
+import{y as e}from"./index-DMYWl0Kz.js";const s=`https://time-manager-app-backend.onrender.com
 
 `;async function i(n,r={}){const a={"Content-Type":"application/json",...r.headers||{}};e.state.accessToken&&(a.Authorization=`Bearer ${e.state.accessToken}`);const o=await fetch(`${s}${n}`,{...r,headers:a}),t=await o.json();if(t.error)throw new Error(t.error);if(o.status===401)throw e.logout(),new Error("Unauthorized");return t}export{i as a};
